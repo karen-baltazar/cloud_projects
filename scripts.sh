e12b2d318168df069e9359bbaa6153dd0e657c13
Karen Baltazar <a01707750@tec.mx>
2023-12-25 00:32:07 -0600

    Copy scripts to AWS instances [Cluster]

--- scripts.sh
+++ scripts.sh
@@ -10,5 +10,14 @@ DNS_NAMES=$(echo "$output" | tail -n +2)
 echo "Step 2: Configuring MySQL Stand-Alone..."
 DNS_NAME=$(echo "$DNS_NAMES" | head -n 1)
 sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME mysql_stand_alone.sh ubuntu@$DNS_NAME:/home/ubuntu
+sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME benchmark.sh ubuntu@$DNS_NAME:/home/ubuntu
+
+# Step 3: Copy master.sh, slave.sh, and benchmark.sh to the AWS instances for MySQL Cluster
+echo "Step 3: Configuring MySQL Cluster..."
+for DNS in $(echo "$DNS_NAMES" | tail -n +2); do
+  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME master.sh ubuntu@$DNS:/home/ubuntu
+  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME slave.sh ubuntu@$DNS:/home/ubuntu
+  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME benchmark.sh ubuntu@$DNS:/home/ubuntu
+done
 
 echo "Automation script completed!"
