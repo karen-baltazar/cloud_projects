f41459efff1d6bd5f633fecc28500713bff26efc
Karen Baltazar <a01707750@tec.mx>
2023-12-26 10:29:45 -0600

    Refactor script to copy master and slave scripts only to respective nodes

--- scripts.sh
+++ scripts.sh
@@ -16,17 +16,25 @@ DNS_NAME=$(echo "$DNS_NAMES" | head -n 1)
 sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/mysql_stand_alone.sh ubuntu@$DNS_NAME:/home/ubuntu
 sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/benchmark.sh ubuntu@$DNS_NAME:/home/ubuntu
 
-# Step 3: Copy master.sh, slave.sh, and benchmark.sh to the AWS instances for MySQL Cluster
-echo "Step 3: Copying master.sh, slave.sh, and benchmark.sh to the AWS instances..."
-for DNS in $(echo "$DNS_NAMES" | tail -n +2); do
-  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/master.sh ubuntu@$DNS:/home/ubuntu
-  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/slave.sh ubuntu@$DNS:/home/ubuntu
-  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/benchmark.sh ubuntu@$DNS:/home/ubuntu
+# Step 3: Copy master_setup.sh, slave_setup.sh, and benchmark.sh to the AWS instances for MySQL Cluster
+echo "Step 3: Copying master_setup.sh, slave_setup.sh, and benchmark.sh to the AWS instances..."
+
+# Extract DNS names for master and slave nodes
+MASTER_DNS=$(echo "$DNS_NAMES" | awk 'NR==2 {print $1}')
+SLAVE_DNS_NAMES=$(echo "$DNS_NAMES" | awk 'NR>=3 && NR<=5 {print $1}')
+
+# Copy master_setup.sh and benchmark.sh to the master node
+sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/master_setup.sh ubuntu@$MASTER_DNS:/home/ubuntu
+sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/benchmark.sh ubuntu@$MASTER_DNS:/home/ubuntu
+
+# Copy slave_setup.sh to all slave nodes
+for DNS in $SLAVE_DNS_NAMES; do
+  sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME db_scripts/slave_setup.sh ubuntu@$DNS:/home/ubuntu
 done
 
 # Step 4: Copying proxy_setup.py to the Proxy instance
 echo "Step 4: Copying proxy_setup.py to the Proxy instance..."
 PROXY_DNS_NAME=$(echo "$output" | tail -n 1)
-sudo scp -o StrictHostKeyChecking=no -i $PROXY_KEY_NAME proxy_setup.py ubuntu@$PROXY_DNS_NAME:/home/ubuntu
+sudo scp -o StrictHostKeyChecking=no -i $KEY_NAME proxy_setup.py ubuntu@$PROXY_DNS_NAME:/home/ubuntu
 
 echo "Automation script completed!"
