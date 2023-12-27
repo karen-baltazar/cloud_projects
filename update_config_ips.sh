7af0225091e1d468c718b234ce6d2c47d1386f8e
Karen Baltazar <a01707750@tec.mx>
2023-12-27 15:24:30 -0600

    Update automatic IP address configuration

--- update_config_ips.sh
+++ update_config_ips.sh
@@ -3,8 +3,9 @@
 # Read private IP addresses from the file private_ips.txt
 private_ip_addresses=($(cat private_ips.txt))
 
-# Path to the db_scripts folder
+# Path to the scripts
 db_scripts_path="db_scripts"
+cloud_scripts_path="cloud_patterns"
 
 # Replace ip addresses in the master_setup.sh script
 sed -i "s/<master_private_ip>/${private_ip_addresses[0]}/g" "${db_scripts_path}/master_setup.sh"
@@ -12,11 +13,19 @@ sed -i "s/<data_node_1_private_ip>/${private_ip_addresses[1]}/g" "${db_scripts_p
 sed -i "s/<data_node_2_private_ip>/${private_ip_addresses[2]}/g" "${db_scripts_path}/master_setup.sh"
 sed -i "s/<data_node_3_private_ip>/${private_ip_addresses[3]}/g" "${db_scripts_path}/master_setup.sh"
 
-# Replace ip addresses in the proxy_setup.py script
-sed -i "s/<master_private_ip>/${private_ip_addresses[0]}/g" proxy_setup.py
-sed -i "s/<data_node_1_private_ip>/${private_ip_addresses[1]}/g" proxy_setup.py
-sed -i "s/<data_node_2_private_ip>/${private_ip_addresses[2]}/g" proxy_setup.py
-sed -i "s/<data_node_3_private_ip>/${private_ip_addresses[3]}/g" proxy_setup.py
-
 # Replace <master_private_ip> in the slave_setup.sh script
 sed -i "s/<master_private_ip>/${private_ip_addresses[0]}/g" "${db_scripts_path}/slave_setup.sh"
+
+# Replace ip addresses in the proxy_setup.py script
+sed -i "s/<master_private_ip>/${private_ip_addresses[0]}/g" "${cloud_scripts_path}/proxy_setup.sh"
+sed -i "s/<data_node_1_private_ip>/${private_ip_addresses[1]}/g" "${cloud_scripts_path}/proxy_setup.sh"
+sed -i "s/<data_node_2_private_ip>/${private_ip_addresses[2]}/g" "${cloud_scripts_path}/proxy_setup.sh"
+sed -i "s/<data_node_3_private_ip>/${private_ip_addresses[3]}/g" "${cloud_scripts_path}/proxy_setup.sh"
+sed -i "s/<proxy_private_ip>/${private_ip_addresses[4]}/g" "${cloud_scripts_path}/proxy_setup.sh"
+
+# Replace ip addresses in the gatekeeper_setup.py script
+sed -i "s/<trusted_host_private_ip>/${private_ip_addresses[6]}/g" "${cloud_scripts_path}/gatekeeper_setup.sh"
+
+# Replace ip addresses in the trusted_host_setup.py script
+sed -i "s/<proxy_private_ip>/${private_ip_addresses[4]}/g" "${cloud_scripts_path}/trusted_host_setup.sh"
+sed -i "s/<trusted_host_private_ip>/${private_ip_addresses[6]}/g" "${cloud_scripts_path}/trusted_host_setup.sh"
